d9c84a8a2b2e105ea801c16b39ea907e4a0ba7ee
Bhajan Dhami <bhajandhami314@gmail.com>
2022-06-10 11:19:56 +0530

    update arithmetic file

--- arithmetic_expressions/arithmetic_example.sh
+++ arithmetic_expressions/arithmetic_example.sh
@@ -1,30 +1,36 @@
 #! /usr/bin/bash
 
-num1=23
-num2=4
+read -p "Enter first Numebr! "num1
+read -p "Enter second Number! "num2
 
 # Addition
 num3=$(($num1+$num2)) # syntax for arithmetic expression $((expression))
+echo "Addition"
 echo $num3
 
 # Subtraction
 num3=$(($num1-$num2))
+echo "Subtraction"
 echo $num3
 
 # Multiplication
 num3=$(($num1*$num2))
+echo "Multiplication"
 echo $num3
 
-# Divition
+# Division
 num3=$(($num1/$num2))
+echo "Division"
 echo $num3
 
 # Exponentiation
 num3=$(($num1**$num2))
+echo "Exponentiation"
 echo $num3
 
 # Modulus
 num3=$(($num1%$num2))
+echo "Modulus"
 echo $num3
 
 
