9d0541c4125bec07e7d010282be74a2e0ddb5049
Bhajan Dhami <bhajandhami314@gmail.com>
2022-06-10 14:12:24 +0530

    add AND and OR example

--- conditional_statements/conditional_statements.sh
+++ conditional_statements/conditional_statements.sh
@@ -13,3 +13,17 @@ elif [ $num1 -lt $num2 ]
 then
 echo "Number1 is less than Number2"
 fi
+
+# use of AND and OR 
+
+read -p "Enter your age " age
+if [ $age -lt 60 -a $age -ge 21 ]  # AND operator -a
+then
+echo "You are an adult!"
+elif [ $age -le 20 -a $age -gt 13 ]
+then
+echo "You are a teen!"
+elif [ $age -le 12 -o $age -ge 60 ] # OR operator -o
+then
+echo "You are neither Adult nor Teen!"
+fi
