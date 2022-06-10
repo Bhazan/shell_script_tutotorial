7d8bb54b97fcf43f2acf930dde7bedba8f424ee3
Bhajan Dhami <bhajandhami314@gmail.com>
2022-06-10 12:04:50 +0530

    move file

---
+++ conditional_statements/conditional_statements.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+read -p "Enter first Number " num1
+read -p "Enter second Number " num2
+
+if [ $num1 -gt $num2 ]
+then
+echo "Number1 is greater than Number2"
+elif [ $num1 -eq $ume2 ]
+then
+echo "Number1 is equal to Number2"
+elif [ $num1 -lt $num2 ]
+then
+echo "Number1 is less than Number2"
+fi
